9be6eee6554e63fa1e1d69ce3c942f7ae6c3430d
Vlad Korotki <vladkorotki92@gmail.com>
2024-05-22 14:34:50 +0300

    add base structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.pngall.com/wp-content/uploads/8/Cooking-Recipe.png'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.pngall.com/wp-content/uploads/8/Cooking-Recipe.png'
+    ),
+  ];
 
+  constructor() {}
+  ngOnInit(): void {}
 }
